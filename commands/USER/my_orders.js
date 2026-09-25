f4ace4ecaaa431f4850f179c0181ad40d58ca785
mkmalakzai <mkmalakzai@gmail.com>
2026-09-25 12:42:10 +0430

    TPL-003: fix My Orders ownership and add product delete UI

--- commands/USER/my_orders.js
+++ commands/USER/my_orders.js
@@ -12,8 +12,15 @@
   <<KEYBOARD
 
   KEYBOARD
-  aliases: 
+  aliases:
   group:
 CMD*/
 
-var os=Bot.getProperty("store_orders")||[],b=[],n=0;for(var i=os.length-1;i>=0&&n<15;i--)if(os[i].user_id==user.telegramid){b.push([{title:"📦 "+os[i].id+" • "+os[i].status,command:"order_view "+os[i].id}]);n++;}b.push([{title:"🏠 Main Menu",command:"main_menu"}]);Bot.sendInlineKeyboard(b,"📦 *MY ORDERS*\n━━━━━━━━━━━━━━\n\n"+(n?"Select an order.":"You haven't placed an order yet."),{parse_mode:"Markdown"});
+var os=Bot.getProperty("store_orders")||[],ids=User.getProperty("my_order_ids")||[],b=[],n=0,uid=String(user.telegramid);
+for(var i=os.length-1;i>=0&&n<15;i--){
+  var owned=String(os[i].user_id)==uid;
+  if(!owned&&ids.indexOf(os[i].id)>=0)owned=true;
+  if(owned){b.push([{title:"📦 "+os[i].id+" • "+os[i].status,command:"order_view "+os[i].id}]);n++;}
+}
+b.push([{title:"🏠 Main Menu",command:"main_menu"}]);
+Bot.sendInlineKeyboard(b,"📦 *MY ORDERS*\n━━━━━━━━━━━━━━\n\n"+(n?"Select an order.":"You haven't placed an order yet."),{parse_mode:"Markdown"});
